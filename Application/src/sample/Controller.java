9567934f04dc41dee9fb0ca79b62871ace898a37
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2016-03-10 00:18:51 +0100

    Takes on GUI to add the table ChillernNavn to listView (controller class)

--- Application/src/sample/Controller.java
+++ Application/src/sample/Controller.java
@@ -1,19 +1,26 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Controller {
 
+    @FXML
+    ListView listView;
+
+    MYSQL_Connection mysql = new MYSQL_Connection("jdbc:mysql://mysql.stud.ntnu.no/cornelgd_databaser", "cornelgd_dbprosj", "1234");
+    Connection myConnection = mysql.getConnection();
+    ResultSet rs;
 
     public void addField(){
-        MYSQL_Connection mysql = new MYSQL_Connection("jdbc:mysql://mysql.stud.ntnu.no/cornelgd_databaser", "cornelgd_dbprosj", "1234");
-        Connection myConnection = mysql.getConnection();
         try {
             PreparedStatement testStatement;
-            boolean found = false;
             String sqlStatement = "SELECT * from ChillernNavn;";
             testStatement = myConnection.prepareStatement(sqlStatement);
-            ResultSet rs;
             rs = testStatement.executeQuery();
             while (rs.next()) {
                 System.out.println(rs.getString(1)); //gets the first column's rows.
@@ -27,8 +34,24 @@ public class Controller {
         }
     }
 
-    public static void main (String[] args){
-        Controller c = new Controller();
-        c.addField();
+    public void printToListView(){
+        String sqlStatement = "SELECT * from ChillernNavn;";
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            PreparedStatement ps = myConnection.prepareStatement(sqlStatement);
+            rs = ps.executeQuery();
+            int i = 0;
+            while (rs.next()){
+                list.add(rs.getString(1));
+            }
+            listView.setItems(FXCollections.observableList(list));
+            System.out.print(list);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initialize(){
+        printToListView();
     }
 }
