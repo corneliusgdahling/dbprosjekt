555da20362aca9b4bd24ff388b810b47a7774159
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2016-03-10 01:19:06 +0100

    Names can be added in real time to database. Database can be updated in real time from within app as well

--- Application/src/sample/Controller.java
+++ Application/src/sample/Controller.java
@@ -1,20 +1,25 @@
 package sample;
 
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 import java.sql.*;
 import java.util.ArrayList;
 
 public class Controller {
 
-    @FXML
-    ListView listView;
+    @FXML private TextField nameField;
+    @FXML private ListView listView;
+    @FXML private Label title;
 
     MYSQL_Connection mysql = new MYSQL_Connection("jdbc:mysql://mysql.stud.ntnu.no/cornelgd_databaser", "cornelgd_dbprosj", "1234");
     Connection myConnection = mysql.getConnection();
     ResultSet rs;
+    ArrayList<String> list = new ArrayList<>();
 
     public void addField(){
         try {
@@ -35,8 +40,13 @@ public class Controller {
     }
 
     public void printToListView(){
+        listView.setItems(FXCollections.observableList(getUpdatedList()));
+    }
+
+
+    public ArrayList<String> getUpdatedList(){
+        list.clear();
         String sqlStatement = "SELECT * from ChillernNavn;";
-        ArrayList<String> list = new ArrayList<>();
         try {
             PreparedStatement ps = myConnection.prepareStatement(sqlStatement);
             rs = ps.executeQuery();
@@ -44,14 +54,35 @@ public class Controller {
             while (rs.next()){
                 list.add(rs.getString(1));
             }
-            listView.setItems(FXCollections.observableList(list));
             System.out.print(list);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            title.setText(rsmd.getColumnName(1));
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return list;
     }
+
 
     public void initialize(){
         printToListView();
+    }
+
+    public void onClickAddName(ActionEvent actionEvent) {
+//        getUpdatedList().add(nameField.getText());
+//        listView.setItems(FXCollections.observableList(list));
+        String sqlStatement = "INSERT INTO ChillernNavn (first_name) VALUES ('"+nameField.getText()+"')";
+        try {
+            PreparedStatement addNameStatement = myConnection.prepareStatement(sqlStatement);
+            addNameStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(list);
+        listView.setItems(FXCollections.observableList(getUpdatedList()));
+    }
+
+    public void onClickupdatedb(ActionEvent actionEvent) {
+        listView.setItems(FXCollections.observableList(getUpdatedList()));
     }
 }
--- Application/src/sample/Main.java
+++ Application/src/sample/Main.java
@@ -2,8 +2,10 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +13,18 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
