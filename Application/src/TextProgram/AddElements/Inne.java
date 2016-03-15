ac3ab8902ee7fdac9e8c2b1d9eb679154711913d
egiluggerud <egiugger@gmail.com>
2016-03-15 16:47:09 +0100

    mekket determineWhere, ute og inne

--- Application/src/TextProgram/AddElements/Inne.java
+++ Application/src/TextProgram/AddElements/Inne.java
@@ -5,55 +5,50 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-/**
- * Innendørs eller ute?
- */
 
 
-public class DetermineWhere {
-	private int forhold;
+public class Inne {
+	
 	private Scanner sc = new Scanner(System.in);
 	
 	
-	public DetermineWhere(){
+	public Inne(){
 		TextProgram.MYSQL_Connection mysql = new TextProgram.MYSQL_Connection("jdbc:mysql://mysql.stud.ntnu.no/cornelgd_databaser", "cornelgd_dbprosj", "1234");
 		Connection myConnection = mysql.getConnection();
-		System.out.println("Enter if the session was completed outside or inside");
-		setForhold_id();
+		
 		try {
 	
 			PreparedStatement determineWhere = myConnection.prepareStatement(getSQLStatement());
-			determineWhere.setInt(1, getForhold_id());
-			if (getForhold_id() < 100){
-				Ute ute = new Ute();
-			} else{
-				Inne inne = new Inne();
-			}
+			determineWhere.setString(1, getAtmosfære());
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
 	
 	
 	public String getSQLStatement(){
-		String sqlStatement = "INSERT INTO Innendørs (Forhold_id) VALUES " + "(?) ";
+		String sqlStatement = "INSERT INTO Innendørs (atmosfære) VALUES " + "(?) ";
 		return sqlStatement;
 	}
 	
 	
-	
-	public void setForhold_id(){
-		System.out.println("\nEnter the forhold_id");
-		forhold = Integer.parseInt(sc.nextLine());
+	public String getAtmosfære(){
+		System.out.println("\nEnter information about the training atmosphere");
+		String atmosfære = sc.nextLine();
+		if (atmosfære.length() < 1){
+			atmosfære = null;
+		}
+		return atmosfære;
 	}
-	public int getForhold_id(){
-		return this.forhold;
-	}
 	
-	
-	
-	
+		
 }
+
+
+
+
+
+
+
+
